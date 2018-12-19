2d3b352544b146b9f4a62b22d77529bd7872b8cf
Júnior Pires <juniorpiresupe@gmail.com>
2018-12-19 14:40:45 -0300

    deploy

--- public/index.php
+++ public/index.php
@@ -20,6 +20,7 @@ session_start();
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
+$app->config('debug', true);
 
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
